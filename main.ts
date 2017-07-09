45ad34f9198395c9918a1880e8d61901e3bdcdef
wanglihui <lihui.wang@tulingdao.com>
2017-07-09 23:51:21 +0800

    记录进程错误

--- main.ts
+++ main.ts
@@ -47,6 +47,10 @@ zone.forkStackTrace()
         if (cluster.isWorker) { 
             process.title = `${config.appName || pkg.name}-worker`;
         }
+        process.on('error', function (err) { 
+            logger.error(err.stack ? err.stack : err);
+            throw err;
+        })
         await API.init(path.join(__dirname, 'api'), config.api);
         await API.startServices(config.listen);
         logger.info(`worker#${process.pid} API listen on ${config.listen}`);
