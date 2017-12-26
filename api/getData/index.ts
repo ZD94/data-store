9afb766f2d9dc329545b8caa452f3dc7d85420d3
hexisen <616082046@qq.com>
2017-12-26 15:26:14 +0800

    delete the test code ,instead of a file

--- api/getData/index.ts
+++ api/getData/index.ts
@@ -2,7 +2,7 @@
  * @Author: Mr.He 
  * @Date: 2017-12-08 18:19:41 
  * @Last Modified by: Mr.He
- * @Last Modified time: 2017-12-26 12:14:31
+ * @Last Modified time: 2017-12-26 15:25:29
  * @content what is the content of this file. */
 
 import cache from "@jingli/cache";
@@ -27,65 +27,3 @@ let getData = {
 };
 
 export default getData;
-
-// setTimeout(async () => {
-
-//     let params = {
-//         type: BudgetType.HOTEL,
-//         channels: ["ctrip-hotel-domestic"],
-//         input: {
-//             checkInDate: "2018-1-22",
-//             checkOutDate: "2018-1-24",
-//             city: "CT_131"
-//         },
-//         step: STEP.FINAL,
-//         data: [],
-//         isAbroad: false
-//     } as DataOrder;
-//     // finalData.createRealTimeData(params, "ctrip-hotel-domestic")
-
-//     setTimeout(async () => {
-//         try {
-//             let result = await getData.getData(params);
-//             console.log("finnaly get the result : ", result);
-
-//         } catch (e) {
-//             console.log(e);
-//         }
-//     }, 6000);
-
-//     // let result = await getData.getData(params);
-//     // console.log("result =====>", result);
-
-// }, 5000);
-
-
-
-
-
-/* setTimeout(async () => {
-
-    let params = {
-        type: BudgetType.TRAFFICT,
-        channels: ["ctrip-hotel-domestic"],
-        input: {
-            leaveDate: "2018-1-22",
-            originPlace: "CT_179",
-            destination: "CT_075"
-        },
-        step: STEP.FINAL,
-        data: [],
-        isAbroad: false
-    } as DataOrder;
-    // finalData.createRealTimeData(params, "ctrip-hotel-domestic")
-
-    // setTimeout(async () => {
-    try {
-        let result = await getData.getData(params);
-        console.log("finnaly get the result : ", result);
-
-    } catch (e) {
-        console.log(e);
-    }
-    // }, 6000);
-}, 4000); */
